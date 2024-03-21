7e18f91302dbd6ff27a614c371e7ba4bf6ef1938
Warren <warreneiserman@gmail.com>
2024-03-21 13:05:23 +0000

    change title

--- knowledge_gpt/main.py
+++ knowledge_gpt/main.py
@@ -26,8 +26,8 @@ MODEL_LIST = ["gpt-3.5-turbo", "gpt-4"]
 # Uncomment to enable debug mode
 # MODEL_LIST.insert(0, "debug")
 
-st.set_page_config(page_title="KnowledgeGPT", page_icon="📖", layout="wide")
-st.header("📖KnowledgeGPT")
+st.set_page_config(page_title="SAPKnowledgeGPT", page_icon="📖", layout="wide")
+st.header("📖SAPCDSKnowledgeGPT")
 
 # Enable caching for expensive functions
 bootstrap_caching()
